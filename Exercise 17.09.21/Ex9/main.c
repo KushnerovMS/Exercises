747d3654d8922d7e87f882551807d685e86be319
KushnerovMS <89263262+KushnerovMS@users.noreply.github.com>
2021-09-22 23:05:03 +0300

    Add files via upload

--- Exercise 17.09.21/Ex9/main.c
+++ Exercise 17.09.21/Ex9/main.c
@@ -3,13 +3,17 @@
 
 const int MAX_SIZE = 1000;
 
+struct box
+{
+    int price;
+    int sweetCount;
+};
+
+void sortBoxes(struct box boxes[], int length);
+
 int main()
 {
-    struct box
-    {
-        int price;
-        int sweetCount;
-    } boxes[MAX_SIZE] = {};
+    struct box boxes[MAX_SIZE] = {};
 
     int boxCount = 0;
     int moneyCount = 0;
@@ -24,27 +28,39 @@ int main()
 
     scanf("%d", &moneyCount);
 
-    int bestBox = 0;
-    int bestBoxCount = 0;
-    int bestSweetCount = 0;
-
-    for(int i = 0; i < boxCount; i ++)
+    int boughtSweetCount = 0;
+    int boughtBoxCount = 0;
+    for(int i = 0; i < boxCount && moneyCount >= boxes[i].price; i++)
     {
-        int boxCount = moneyCount / (boxes[i].price);
-        if(boxCount == bestBoxCount && bestSweetCount < boxCount*boxes[i].sweetCount)
-        {
-            bestSweetCount = boxCount*boxes[i].sweetCount;
-            bestBox = i;
-        }
-        else if(boxCount > bestBoxCount)
-        {
-            bestBoxCount = boxCount;
-            bestSweetCount = bestBoxCount*boxes[i].sweetCount;
-            bestBox = i;
-        }
+        boughtSweetCount += boxes[i].sweetCount;
+        boughtBoxCount ++;
+        moneyCount -= boxes[i].price;
     }
 
-    printf("%d %d", bestBoxCount, bestBoxCount*boxes[bestBox].sweetCount);
+    printf("%d %d", boughtBoxCount, boughtSweetCount);
 
     return 0;
+}
+
+void sortBoxes(struct box boxes[], int length)
+{
+    int iMin = 0;
+    for(int i = 0; i < length - 1; i++)
+    {
+        iMin = i;
+
+        for(int j = i + 1; j < length; j ++)
+        {
+            if(boxes[j].price < boxes[iMin].price ||
+            (boxes[j].price == boxes[iMin].price && boxes[j].sweetCount < boxes[iMin].sweetCount))
+                iMin = j;
+        }
+
+        if(iMin != i)
+        {
+            struct box a = boxes[iMin];
+            boxes[iMin] = boxes[i];
+            boxes[i] = a;
+        }
+    }
 }
